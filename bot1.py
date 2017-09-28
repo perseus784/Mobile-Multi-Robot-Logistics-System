379abb0bd452c088d362ed75d8493795f6a16f0a
Paarvendhan <ipaar3@gmail.com>
2017-09-28 04:02:49 +0000

    bot1.py edited online with Bitbucket

--- bot1.py
+++ bot1.py
@@ -6,12 +6,14 @@ import serial
 
 mqttc=mqtt.Client()
 host='52.90.36.67'
-ard_dire={'u':1,'l':2,'r':3,'d':4}
-port = '/dev/ttyS0'
+ard_dire={'s':0,'u':1,'l':2,'r':3,'d':4}
+port = '/dev/ttyACM0'
 ard = serial.Serial(port,9600,timeout=5)
+ser= serial.Serial('/dev/ttyS0',9600,timeout=5)
 mqttc.connect(host,1883,60)
 mqttc.loop_start()
 global an
+
 def on_message_print(client, userdata, message):
     global an
     an=(message.payload)
@@ -39,49 +41,72 @@ def logger(val):
     mqttc.publish("log",val, 2)
     pass
 
-prev = ''
-
+prevd = ''
+prevr = ''
+prevl = ''
+previousid=''
 def arucoid(ide, dire,flag):
     global an
-    global prev
+    global previousid
+    global prevd,prevl,prevr
     print ide, dire
-
+    logger('@rasp {}reached the function'.format(ide))
+    #ard.write(str(ard_dire['u']))
     while flag:
-        line = ard.read(12)  # read a '\n' terminated line
+
+        while 1:
+            line = ser.read(12)
+            if line:
+                logger('@rasp found marker id')
+                break
+
         # print line
         line1 = csvfunc(line)
-        if str(ide)==str(line1):
-            logger('@rasp found marker id {}'.format(ide))
-            if dire=='r' or 'l':
+        print 'line',line1
+        ard.write(str(ard_dire['s']))
 
-                #make turn
-                ard.write(str(ard_dire[dire]))
-                logger('@arduino sent to arduino')
-                time.sleep(2)
-                msg = ard.readline(ard.inWaiting())
-                #go forward
-                ard.write(str(1))
-                logger('@arduino sent to arduino')
-                time.sleep(2)
-                msg = ard.readline(ard.inWaiting())
-            elif dire=='d':
-                if prev != 'd':
+        if str(ide)==str(line1[0]):
+            print 'here'
+            logger('@rasp FOUND marker id {}'.format(ide))
+            if dire=='s':
+                break
+            if dire==('r' or 'l'):
+                if dire != (prevl or prevr):
+                    # make turn
                     ard.write(str(ard_dire[dire]))
                     logger('@arduino sent to arduino')
                     time.sleep(2)
-                    msg = ard.readline(ard.inWaiting())
-                else:
-                    prev ='d'
+                    msg = ser.readline(ard.inWaiting())
+                    # go forward
                     ard.write(str(1))
                     logger('@arduino sent to arduino')
                     time.sleep(2)
-                    msg = ard.readline(ard.inWaiting())
+                    msg = ser.readline(ard.inWaiting())
+                else:
+                    if dire=='r':prevr='r'
+                    elif dire=='l':prevl='l'
+                    ard.write(str(1))
+                    logger('@arduino sent to arduino')
+                    msg = ser.readline(ard.inWaiting())
+
+            elif dire=='d':
+                if prevd != 'd':
+                    ard.write(str(ard_dire['l']))
+                    time.sleep(3)
+                    ard.write(str(ard_dire['l']))
+                    logger('@arduino sent to arduino')
+                    msg = ser.readline(ard.inWaiting())
+                else:
+                    prevd ='d'
+                    ard.write(str(1))
+                    logger('@arduino sent to arduino')
+                    time.sleep(2)
+                    msg = ser.readline(ard.inWaiting())
+
             elif dire=='u':
                 ard.write(str(ard_dire[dire]))
                 logger('@arduino sent to arduino')
-                time.sleep(2)
-                msg = ard.readline(ard.inWaiting())
-
+                msg = ser.readline(ard.inWaiting())
             flag = False
 
 if __name__ =="__main__":
@@ -94,7 +119,7 @@ if __name__ =="__main__":
         logger('@rasp received marker {} from cps'.format(ids))
 
         arucoid(ids, dire,flag=True)
-        time.sleep(3)
+        ard.write(str(ard_dire['s']))
         logger('@rasp found one marker moving to next marker')
         mqttc.publish("cps1", ids, 2)
         print "sending to cps"
