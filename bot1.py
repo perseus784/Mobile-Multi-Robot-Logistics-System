15e86fb9a45a0abba0abd7ed172722aa4df17836
Paarvendhan <ipaar3@gmail.com>
2017-10-02 07:33:54 +0000

    bot1.py edited online with Bitbucket

--- bot1.py
+++ bot1.py
@@ -14,16 +14,19 @@ mqttc.connect(host,1883,60)
 mqttc.loop_start()
 global an
 
+#mqtt 
 def on_message_print(client, userdata, message):
     global an
     an=(message.payload)
     print an
     client.disconnect()
 
+#encryption
 def encoder(ids,direc):
     ctx=str(ids)+'||'+direc
     return ctx
 
+#crossreferencing rfid values to id's
 def csvfunc(line):
     endlist = []
     with open('rpiref.csv', 'rb') as csvfile:
@@ -33,10 +36,12 @@ def csvfunc(line):
                 endlist.append(row['markerid'])
     return endlist
 
+#decryption
 def decoder(ctx):
     lis=ctx.split('|')
     return lis[0],lis[-1]
 
+#logging survey
 def logger(val):
     mqttc.publish("log",val, 2)
     pass
@@ -45,6 +50,8 @@ prevd = ''
 prevr = ''
 prevl = ''
 previousid=''
+
+#main function
 def arucoid(ide, dire,flag):
     global an
     global previousid
@@ -52,24 +59,31 @@ def arucoid(ide, dire,flag):
     print ide, dire
     logger('@rasp {}reached the function'.format(ide))
     #ard.write(str(ard_dire['u']))
+    #while node not reached
     while flag:
-
+        
+        #checking for the rfid input
         while 1:
             line = ser.read(12)
             if line:
                 logger('@rasp found marker id')
                 break
-
-        # print line
+        #ones the rfid is read....
+        
         line1 = csvfunc(line)
         print 'line',line1
+        #telling arduino to stop
         ard.write(str(ard_dire['s']))
-
+        
+        #taking actions according to directions.....
         if str(ide)==str(line1[0]):
             print 'here'
             logger('@rasp FOUND marker id {}'.format(ide))
+            #USE THE SAME LOGIC
+            #for stopping
             if dire=='s':
                 break
+            #for left or right
             if dire==('r' or 'l'):
                 if dire != (prevl or prevr):
                     # make turn
@@ -88,7 +102,8 @@ def arucoid(ide, dire,flag):
                     ard.write(str(1))
                     logger('@arduino sent to arduino')
                     msg = ser.readline(ard.inWaiting())
-
+            
+            #for reverse
             elif dire=='d':
                 if prevd != 'd':
                     ard.write(str(ard_dire['l']))
@@ -102,7 +117,8 @@ def arucoid(ide, dire,flag):
                     logger('@arduino sent to arduino')
                     time.sleep(2)
                     msg = ser.readline(ard.inWaiting())
-
+            
+            #for forward
             elif dire=='u':
                 ard.write(str(ard_dire[dire]))
                 logger('@arduino sent to arduino')
@@ -113,12 +129,16 @@ if __name__ =="__main__":
     logger('@rasp sucessfully launched robot')
 
     while 1:
+        #getting input from CPS
         subscribe.callback(on_message_print, "rpi1", hostname=host)
         print "received marker from cps", an
         ids, dire = decoder(an)
         logger('@rasp received marker {} from cps'.format(ids))
-
+        
+        #giving node and direcion to the fucntion
         arucoid(ids, dire,flag=True)
+        
+        #giving information to CPS that node is reached
         ard.write(str(ard_dire['s']))
         logger('@rasp found one marker moving to next marker')
         mqttc.publish("cps1", ids, 2)
