ae46cffac9fcd185ea4954765c42e51873bfe491
Paarvendhan <ipaar3@gmail.com>
2017-10-05 19:08:19 +0000

    nodemcu.py edited online with Bitbucket

--- nodemcu.py
+++ nodemcu.py
@@ -13,13 +13,12 @@ sta_if.active()
 sta_if.ifconfig()
 subs_temp = ''
 
-ide_lis=[b'310066832BFF', b'31006D651A23', b'3100432FE5B8', 
-b'310042FBA52D', b'310046F120A6', b'3100434883B9', b'310046F2BE3B',
-b'31006D69AE9B', b'310046F1199F', b'3100433C1D53', b'3100471B305D',
-b'3100668313C7', b'310066830ADE',
-b'3100668302D6', b'310066831DC9', b'3100668316C2',
-b'3100471B375A', b'3100471B3A57', b'31006D71E0CD', b'31006D7FB497']
-
+ide_lis = [b'310066832BFF', b'31006D651A23', b'3100432FE5B8',
+           b'310042FBA52D', b'310046F120A6', b'3100434883B9', b'310046F2BE3B',
+           b'31006D69AE9B', b'310046F1199F', b'3100433C1D53', b'3100471B305D',
+           b'3100668313C7', b'310066830ADE',
+           b'3100668302D6', b'310066831DC9', b'3100668316C2',
+           b'3100471B375A', b'3100471B3A57', b'31006D71E0CD', b'31006D7FB497']
 
 # serial com
 rfid = ser(0, 9600)
@@ -62,23 +61,37 @@ def decoder(ctx):
     lis = ctx.split('|')
     return lis[0], lis[-1]
 
-def action(ids,dire,flag):
+
+def rfidtonode(rfid):
+    count = 0
+    logger('@rasp {}'.format(rfid))
+    for ide in ide_lis:
+
+        if rfid == str(ide):
+            ard.write(str(count))
+            break
+        count += 1
+    return count
+
+
+def action(ids, dire, flag):
     time.sleep(2)
-    logger('@rasp reached the function')
-    
+    logger('@rasp reached the function {}'.format(str(dire)))
+
     while flag:
         # checking for the rfid input
         while 1:
             line = rfid.read()
             if line:
-                time.sleep(2)
-                logger('@rasp found marker id')
+                logger('@rasp READ MARKER ID')
                 break
-                # ones the rfid is read...
 
-        flag=False
+        node = rfidtonode(line)
+        time.sleep(2)
+        logger('@rasp found node {}'.format(node))
+        flag = False
+        
     pass
-
 
 
 if __name__ == '__main__':
@@ -95,14 +108,11 @@ if __name__ == '__main__':
     logger('subscription over')
     time.sleep(2)
     while 1:
-        
-        logger('entered loop')
-        time.sleep(2)
         client.wait_msg()
         logger(subs_temp)
         ids, dire = decoder(str(subs_temp))
         time.sleep(2)
         logger('@rasp received marker {} from cps'.format(ids))
-        action(ids=ids,dire=dire,flag=True)
+        action(ids=ids, dire=dire, flag=True)
 
     c.disconnect()
